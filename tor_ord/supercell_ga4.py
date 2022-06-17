f34ef56b264742324c77241666c68145cb4f2d46
Kristin Ludwicki <krl4005@med.cornell.edu>
2022-06-17 17:24:23 -0400

    ga4

--- tor_ord/supercell_ga4.py
+++ tor_ord/supercell_ga4.py
@@ -655,11 +655,11 @@ def start_ga(pop_size=200, max_generations=100):
     toolbox.register('mutate', _mutate)
 
     # To speed things up with multi-threading
-    #p = Pool()
-    #toolbox.register("map", p.map)
+    p = Pool()
+    toolbox.register("map", p.map)
 
     # Use this if you don't want multi-threading
-    toolbox.register("map", map)
+    #toolbox.register("map", map)
 
     # 2. Calling the GA to run
     final_population = run_ga(toolbox)
