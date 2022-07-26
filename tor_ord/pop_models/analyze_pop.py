63f7a383d8550891f91597ccb3c167b992819431
Kristin Ludwicki <krl4005@med.cornell.edu>
2022-07-26 15:39:12 -0400

    run 2500 models again

--- tor_ord/pop_models/analyze_pop.py
+++ tor_ord/pop_models/analyze_pop.py
@@ -150,8 +150,115 @@ def plot_data(d, c):
 
     return(AP_label)
 
+def detect_EAD(t, v):
+    #find slope
+    slopes = []
+    for i in list(range(0, len(v)-1)):
+        m = (v[i+1]-v[i])/(t[i+1]-t[i])
+        slopes.append(round(m, 2))
+
+    #find rises
+    pos_slopes = np.where(slopes > np.float64(0.0))[0].tolist()
+    pos_slopes_idx = np.where(np.diff(pos_slopes)!=1)[0].tolist()
+    pos_slopes_idx.append(len(pos_slopes)) #list must end with last index
+
+    #pull out groups of rises (indexes)
+    pos_groups = []
+    pos_groups.append(pos_slopes[0:pos_slopes_idx[0]+1])
+    for x in list(range(0,len(pos_slopes_idx)-1)):
+        g = pos_slopes[pos_slopes_idx[x]+1:pos_slopes_idx[x+1]+1]
+        pos_groups.append(g)
+
+    #pull out groups of rises (voltages and times)
+    vol_pos = []
+    tim_pos = []
+    for y in list(range(0,len(pos_groups))):
+        vol = []
+        tim = []
+        for z in pos_groups[y]:
+            vol.append(v[z])
+            tim.append(t[z])
+        vol_pos.append(vol)
+        tim_pos.append(tim) 
+
+    #Find EAD given the conditions (voltage>-70 & time>100)
+    EADs = []
+    EAD_vals = []
+    for k in list(range(0, len(vol_pos))):
+        if np.mean(vol_pos[k]) > -70 and np.mean(tim_pos[k]) > 100:
+            EAD_vals.append(tim_pos[k])
+            EAD_vals.append(vol_pos[k])
+            EADs.append(max(vol_pos[k])-min(vol_pos[k]))
+
+    #Report EAD 
+    if len(EADs)==0:
+        info = "no EAD"
+        result = 0
+    else:
+        info = "EAD:", round(max(EADs))
+        result = 1
+    
+    return result
+
+def detect_RF(t, v):
+
+    #find slopes
+    slopes = []
+    for i in list(range(0, len(v)-1)):
+        m = (v[i+1]-v[i])/(t[i+1]-t[i])
+        slopes.append(round(m, 1))
+
+    #find times and voltages at which slope is 0
+    zero_slopes = np.where(slopes == np.float64(0.0))[0].tolist()
+    zero_slopes_idx = np.where(np.diff(zero_slopes)!=1)[0].tolist()
+    zero_slopes_idx.append(len(zero_slopes)) #list must end with last index
+
+    #pull out groups of zero slope (indexes)
+    zero_groups = []
+    zero_groups.append(zero_slopes[0:zero_slopes_idx[0]+1])
+    for x in list(range(0,len(zero_slopes_idx)-1)):
+        g = zero_slopes[zero_slopes_idx[x]+1:zero_slopes_idx[x+1]+1]
+        zero_groups.append(g)
+
+    #pull out groups of zero slopes (voltages and times)
+    vol_pos = []
+    tim_pos = []
+    for y in list(range(0,len(zero_groups))):
+        vol = []
+        tim = []
+        for z in zero_groups[y]:
+            vol.append(v[z])
+            tim.append(t[z])
+        vol_pos.append(vol)
+        tim_pos.append(tim) 
+
+
+    #Find RF given the conditions (voltage<-70 & time>100)
+    no_RF = []
+    for k in list(range(0, len(vol_pos))):
+        if np.mean(vol_pos[k]) < -70 and np.mean(tim_pos[k]) > 100:
+            no_RF.append(tim_pos[k])
+            no_RF.append(vol_pos[k])
+
+    #Report EAD 
+    if len(no_RF)==0:
+        info = "Repolarization failure!"
+        result = 1
+    else:
+        info = "normal repolarization - resting membrane potential from t=", no_RF[0][0], "to t=", no_RF[0][len(no_RF[0])-1]
+        result = 0
+    return result
+
+def detect_APD(t, v, apd90_base):
+    APD90_i = calc_APD(t, v, 90)
+    APD90_error = (APD90_i - apd90_base)/(APD90_i)*100
+    if APD90_error < 40:
+        result_APD = 0
+    else:
+        result_APD = 1
+    return(result_APD)
 #%% read in data
-path = 'c:\\Users\\Kristin\\Desktop\\pop_models\\'
+path = 'c:\\Users\\Kristin\\Desktop\\pop_models\\analysis1\\'
 data = pd.read_csv(path+"data.csv")
 
 base_data = data.iloc[:, list(range(0,10))].copy(deep=False)
@@ -187,6 +294,29 @@ print(filtered_AP_labels)
 
 filtered_AP_immune = plot_data(filtered_immune_data, 'blue')
 plt.savefig(path + 'filtered_immunized.png')
+
+#%% Quantify EAD base
+def count_abnormal_APs(d):
+    result = []
+
+    for i in list(range(0, len(d['t_ical']))):
+        t = eval(d['t_ical'].tolist()[i])
+        v = eval(d['v_ical'].tolist()[i])
+        EAD = detect_EAD(t,v)
+        RF = detect_RF(t,v)
+
+        if EAD==1 or RF==1:
+            result.append(1)
+        else:
+            result.append(0)
+    return(result)
+
+abnormal_baseline = count_abnormal_APs(filtered_data)
+print(abnormal_baseline.count(1))
+
+abnormal_immune = count_abnormal_APs(filtered_immune_data)
+print(abnormal_immune.count(1))
+    
 
 # %% Calculate Variance
 vari = []
